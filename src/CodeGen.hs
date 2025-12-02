06b4ddcbb73a27c2c5796732ea284fdd6a4a5491
Fürkhtmensch <42923229+Furkhtmensch@users.noreply.github.com>
2025-12-02 23:35:01 +0000

    Extended transIR for most intermediate code instructions
    Many things to-do, including:
    - Allocation of registers;
    - Differentiation of arithmetic operations (between Integer and Float - this will require changes in the IR.hs file);
    - Creation of composite functions (POW, CONCAT, PRINT and READ).

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -10,8 +10,6 @@ data Location = Reg Temp
               | Stack Offset
               | Global String
 
-
-
 type Temp = String
 type Offset = Int
 type Count = (Int, Int, Int)
@@ -24,6 +22,7 @@ newOffset n = do (offset, temp, dataCounter) <- get
 newTemp :: State Count ()
 newTemp = do (offset, temp, dataCounter) <- get
           put (offset, temp + 1, dataCounter)
+          return (if (temp < 10) then ("t" ++ (show temp)) else ("s" ++ (show (temp - 10))))
 
 popOffset :: Int -> State Count ()
 popOffset n = do (offset, temp, dataCounter) <- get
@@ -33,23 +32,35 @@ popTemp :: State Count ()
 popTemp = do (offset, temp, dataCounter) <- get
           put (offset, temp - 1, dataCounter)
 
-
 nextLabel :: [Instr] -> String -> Bool
-nextLabel _ _ = False
 nextLabel (LABEL l1 :_) l2 = l1 == l2
-
-
+nextLabel _ _ = False
 
 transMips :: [Instr] -> State Count [String]
 transMips instr = do fillData instr
                      return transIR instr
 
-
-
-
 transIR :: [Instr] -> State Count [String]
-transIR [] = return [""]
-transIR (COND EQ t1 t2 l1 l2:rest) | nextLabel rest l1 = ["beq " + t1 + ", " + t2 + ", " + l1] ++ transIR rest
-                                   | nextLabel rest l2 = ["bne " + t1 + ", " + t2 + ", " + l2] ++ transIR rest
-                                   | otherwise         = ["beq " + t1 + ", " + t2 + ", " + l1
+transIR [] = return []
+transIR ((COND EQ t1 t2 l1 l2):remainder) | nextLabel remainder l2 = ["beq " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ (transIR remainder)
+                                          | nextLabel remainder l1 = ["bne " ++ t1 ++ ", " ++ t2 ++ ", " ++ l2] ++ (transIR remainder)
+                                          | otherwise              = ["beq " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ ["j " ++ l2] ++ (transIR remainder)
+transIR ((COND NE t1 t2 l1 l2):remainder) | nextLabel remainder l2 = ["bne " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ (transIR remainder)
+                                          | nextLabel remainder l1 = ["beq " ++ t1 ++ ", " ++ t2 ++ ", " ++ l2] ++ (transIR remainder)
+                                          | otherwise              = ["bne " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ ["j " ++ l2] ++ (transIR remainder)
+transIR ((COND LT t1 t2 l1 l2):remainder) | nextLabel remainder l2 = ["blt " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ (transIR remainder)
+                                          | nextLabel remainder l1 = ["bge " ++ t1 ++ ", " ++ t2 ++ ", " ++ l2] ++ (transIR remainder)
+                                          | otherwise              = ["blt " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ ["j " ++ l2] ++ (transIR remainder)
+transIR ((COND LE t1 t2 l1 l2):remainder) | nextLabel remainder l2 = ["ble " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ (transIR remainder)
+                                          | nextLabel remainder l1 = ["bgt " ++ t1 ++ ", " ++ t2 ++ ", " ++ l2] ++ (transIR remainder)
+                                          | otherwise              = ["ble " ++ t1 ++ ", " ++ t2 ++ ", " ++ l1] ++ ["j " ++ l2] ++ (transIR remainder)
+transIR ((LABEL l):remainder) = [l ++ ":"] ++ (transIR remainder)
+transIR ((JUMP l):remainder) = ["j " ++ l] ++ (transIR remainder)
+transIR ((OP ADD t1 t2 t3):remainder) = ["add " ++ t1 ++ ", " ++ t2 ++ ", " ++ t3] ++ (transIR remainder)
+transIR ((OP SUB t1 t2 t3):remainder) = ["sub " ++ t1 ++ ", " ++ t2 ++ ", " ++ t3] ++ (transIR remainder)
+transIR ((OP MUL t1 t2 t3):remainder) = ["mul " ++ t1 ++ ", " ++ t2 ++ ", " ++ t3] ++ (transIR remainder)
+transIR ((OP DIV t1 t2 t3):remainder) = ["div " ++ t1 ++ ", " ++ t2 ++ ", " ++ t3] ++ (transIR remainder)
+transIR ((MOVE t1 t2):remainder) = ["move " ++ t1 ++ ", " ++ t2] ++ (transIR remainder)
+transIR ((MOVEI t1 k):remainder) = ["li " ++ t1 ++ ", " ++ k] ++ (transIR remainder)
+-- FALTA FAZER VERSOES DIFERENTES PARA FLOAT E INT!
 -- falta escolhers registos
