9afcaabd4e46fd1dbd4972cc183c73dd78dcff8e
Gonçalo <goncalobranquinhom@gmail.com>
2025-12-08 16:03:33 +0000

    update

--- examples/ex4.adb
+++ examples/ex4.adb
@@ -1,24 +1,17 @@
 procedure Main is
-    x : Integer := 5;
     begin
-        Put_Line("A resposta local é: ");
-        x := 7;
         declare
-            a : Integer := 2;
-            b : Integer := 3;
-            x : Integer := 2;
-            y : Integer := 3;
-            p : String := "ola";
             begin
-                x := a+b;
-                y := a-b;
-              
+              declare
+              begin
+
+              end;
+            end;
+            declare
+            begin
+
             end;
         declare
-          k : String;
         begin
-            k := "ola" & "adeus" & "oi";
-            Put_Line(k);
         end;
-        Put_Line("A resposta pós local é: ");
     end Main;
