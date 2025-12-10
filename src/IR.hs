3d47981f13e53e5231124d1857b9cc8021cd95ef
Fürkhtmensch <42923229+Furkhtmensch@users.noreply.github.com>
2025-12-10 00:58:49 +0000

    Update IR.hs

--- src/IR.hs
+++ src/IR.hs
@@ -424,9 +424,9 @@ transExp (Not cond1) table dest = do label1 <- newLabel
                                      code1 <- transCond (Not cond1) table label1 label2
                                      return (code1 ++ [LABEL label1, MOVEI dest (TInt 1), JUMP label3, LABEL label2, MOVEI dest (TInt 0), LABEL label3])
 
-transExp (ToStr id) table dest = do t1 <- newTemp
+transExp (ToStr exp) table dest = do t1 <- newTemp
                                     addTable t1 4 False
-                                    code1 <- transExp (Var id) table t1
+                                    code1 <- transExp (exp) table t1
                                     popTemp 1
                                     typ <- getVarTyp
                                     return (code1 ++ [IR.TOSTR typ dest t1])
